44419e61acbc88489cd60470b3352d0ca7f940a1
Personal <sk7627577@gmail.com>
2026-04-06 15:58:33 +0530

    fix: Adjust heading and paragraph styles for consistency

--- src/app/(Landing-page)/components/cards/WhoUseEazotelCard.tsx
+++ src/app/(Landing-page)/components/cards/WhoUseEazotelCard.tsx
@@ -7,10 +7,10 @@ const WhoUseEazotelCard: React.FC<
   return (
     <div className="bg-background-1 border-8 border-white rounded-xl overflow-hidden  ">
       <div className="px-5.25 py-6">
-        <h3 className="text-primary md:text-xl text-[12px] font-semibold mb-2">
+        <h3 className="text-primary text-xl font-semibold mb-2">
           {title}
         </h3>
-        <p className="text-secondary max-md:text-[10px]">{description}</p>
+        <p className="text-secondary ">{description}</p>
       </div>
       <div
         className={`w-full relative aspect-[4/2.35] rounded-[10px] overflow-hidden mt-5 border-2 border-white ${imageWrapperClassName}`}
